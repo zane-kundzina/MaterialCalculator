46a1704241a63a14b5435bda5059b990fe116328
Zane Kundziņa <zane.gaiduka@gmail.com>
2021-07-03 13:36:47 +0300

    Add project files.

---
+++ MaterialCalculator.Data/Calculator.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MaterialCalculator.Data
+{
+    public class Calculator
+    {
+        public List<Material> materials { get; set; } = new List<Material>();
+
+        public void NewMaterialList()
+        {
+            // method code....
+            // saves/exports current list of materials (optionally) and clears it afterwards; 
+        }
+
+        public void AddMaterial(Material material)
+        {
+            // method code....
+            materials.Add(material);
+        }
+
+        public double CalculateMaterialAmount()
+        {
+            // method code....
+            return 0.0;
+        }
+
+        public double CalculateMaterialWeight()
+        {
+            // method code....
+            return 0.0;
+        }
+
+        public double CalculateTotalWeight()
+        {
+            // method code....
+            return 0.0;
+        }
+
+        public double CalculatePercentageOfLoad()
+        {
+            // method code....
+            return 0.0;
+        }
+
+        public string IsLoadFull()
+        {
+            // method code....
+            return string.Empty;
+        }
+
+
+    }
+}
---
+++ MaterialCalculator.Data/Material.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MaterialCalculator.Data
+{
+    public class Material
+    {
+        public string Type { get; private set; }
+        public string Size { get; private set; }
+        public double PieceSize { get; private set; }
+        public double NumberOfPieces { get; set; }
+        public double WeightPerUnitKg { get; private set; }
+
+        public Material(string type, string size, double pieceSize, double weightPerUnitKg )
+        {
+            Type = type;
+            Size = size;
+            PieceSize = pieceSize;
+            WeightPerUnitKg = weightPerUnitKg;
+        }
+
+
+    }
+}
